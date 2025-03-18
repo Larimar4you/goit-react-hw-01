008b347f8b4b61dbc81e5c915dcf7cc326c2f127
Larimar4you <Zajanezlaja@gmail.com>
2025-03-18 23:57:39 +0100

    m

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,29 @@
+const Profile = ({ name, tag, location, image, stats }) => {
+  return (
+    <>
+      <div>
+        <div>
+          <img src={image} alt="User avatar" />
+          <p>{name}</p>
+          <p>@pmarica</p>
+          <p>Salvador, Brasil</p>
+        </div>
+        <ul>
+          <li>
+            <span>Followers</span>
+            <span>1000</span>
+          </li>
+          <li>
+            <span>Views</span>
+            <span>2000</span>
+          </li>
+          <li>
+            <span>Likes</span>
+            <span>{stats.followers}</span>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+export default Profile;
