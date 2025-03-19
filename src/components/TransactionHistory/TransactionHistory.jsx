43bcd71f398fd4fcc63ca1aa5452f5c1e85e9048
Larimar4you <Zajanezlaja@gmail.com>
2025-03-20 00:33:21 +0100

    n

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import css from './TransactionHistory.module.css';
 
 const TransactionHistory = ({ items }) => {
-  return <div>TransactionHistory</div>;
+  return (
+    <table className={css.transactionsTable}>
+      <thead className={css.transactionsTableHead}>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody className={css.transactionsTableBody}>
+        {items.map(({ id, type, amount, currency }) => {
+          return (
+            <tr key={id}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 };
 
 export default TransactionHistory;
