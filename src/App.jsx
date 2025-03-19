2d63f40da1adc8916461e7d62d026e9618a2759c
Larimar4you <Zajanezlaja@gmail.com>
2025-03-19 16:33:13 +0100

    history

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import userData from './components/Profile/userData.json';
 import FriendList from './components/friendList/FriendList';
 import friends from './components/friendList/friends.json';
 
-import transactions from './components/TransactionHistory/transactions.json';
 import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import transactions from './components/TransactionHistory/transactions.json';
 
 const App = () => {
   return (
