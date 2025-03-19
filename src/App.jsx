75619a54578b05e34950ce5b30a6d8922111ffe2
Larimar4you <Zajanezlaja@gmail.com>
2025-03-19 15:06:11 +0100

    com

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Profile from './components/Profile/Profile';
 import userData from './components/profile/userData.json';
 
-import FriendList from './components/friendList/FriendList';
-import friends from './components/friendList/friends.json';
+import FriendList from './components/friendList/FriendListItem/FriendList';
+import friends from './components/friendList/FriendListItem/friends.json';
 
-import TransactionHistory from './components/transactionHistory/TransactionHistory';
+import TransactionHistory from './components/transactionHistory/TransactionHistory.jsx';
 import transactions from './components/transactionHistory/transactions.json';
 
 const App = () => {
---
+++ src/components/friendList/FriendListItem/FriendList.jsx
@@ -0,0 +1,22 @@
+import FriendListItem from '../friendList/friendListItem/FriendListItem';
+import {
+  friendContainer,
+  friendsList,
+  friendsListItem,
+} from './FriendList.module.css';
+
+function FriendList({ friends }) {
+  return (
+    <div className={friendContainer}>
+      <ul className={friendsList}>
+        {friends.map(friend => (
+          <li className={friendsListItem} key={friend.id}>
+            <FriendListItem friend={friend} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default FriendList;
