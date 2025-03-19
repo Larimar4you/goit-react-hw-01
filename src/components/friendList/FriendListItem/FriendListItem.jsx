63b645e6dfbf6b96b663bbab23655c75a660a9ba
Larimar4you <Zajanezlaja@gmail.com>
2025-03-19 21:57:01 +0100

    n

--- src/components/friendList/FriendListItem/FriendListItem.jsx
+++ src/components/friendList/FriendListItem/FriendListItem.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import styles from './FriendListItem.module.css';
 
-const FriendListItem = ({}) => {
+const FriendListItem = () => {
   return <div>FriendListItem</div>;
 };
 
