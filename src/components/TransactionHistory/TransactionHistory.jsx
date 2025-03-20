290c051a9eaec6c84f2e017fbce60f72bbe61532
Larimar4you <Zajanezlaja@gmail.com>
2025-03-20 01:08:02 +0100

    new

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -3,7 +3,7 @@ import styles from './TransactionHistory.module.css';
 
 const TransactionHistory = ({ items }) => {
   return (
-    <table className={styles.transactionsTable}>
+    <table className={styles.table}>
       <thead>
         <tr>
           <th>Type</th>
